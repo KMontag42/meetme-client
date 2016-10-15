7682d0a282bda9f6e2f5bccc7b0cb7fdc85435ea
kyle <thekylemontag@gmail.com>
2016-10-15 13:58:12 -0400

    update some files

--- components/FindView/FindView.js
+++ components/FindView/FindView.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  TouchableHighlight
+} from 'react-native';
 import QRCodeScreen from '../../lib/QRCodeScreen';
 import Styles from '../../Styles';
 
 export default class FindView extends Component {
+  onSuccess(barcode) {
+    alert(barcode);
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <View style={Styles.container}>
-        <QRCodeScreen/>
+        <QRCodeScreen onSuccess={() => this.onSuccess()} />
+        <TouchableHighlight onPress={}>
+
+        </TouchableHighlight>
       </View>
     )
   }
