1dacc909e4a84cf1bb08e17455d99e09a0ab6bf1
kyle <thekylemontag@gmail.com>
2016-10-15 17:50:41 -0400

    update index.ios to match android

--- index.ios.js
+++ index.ios.js
@@ -7,47 +7,57 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
+  TouchableHighlight,
   Text,
-  View
+  Navigator
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Routes from './routes';
 
 export default class meetme extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={Routes[0]}
+        initialRouteStack={Routes}
+        renderScene={ (route, navigator) => <route.Component navigator={navigator} {...route.props}/>}
+        navigationBar={
+          <Navigator.NavigationBar
+            routeMapper={{
+              LeftButton: (route, navigator, index, navState) =>
+              {
+                if (route.index === 0) {
+                  return null;
+                } else {
+                  return (
+                    <TouchableHighlight onPress={() => navigator.pop()}>
+                      <Icon name="chevron-left" size={30} color="#eee"
+                            style={{padding: 10}}/>
+                    </TouchableHighlight>
+                  );
+                }
+              },
+              RightButton: (route, navigator, index, navState) =>
+              {
+                if (route.index === 1) {
+                  return null;
+                }
+                return (
+                  <TouchableHighlight onPress={() => navigator.push(Routes[1])}>
+                    <Icon name="user" size={30} color="#eee"
+                          style={{padding: 10}}/>
+                  </TouchableHighlight>
+                );
+              },
+              Title: (route, navigator, index, navState) =>
+              { return (<Text style={{color: '#eee'}}></Text>); },
+            }}
+            style={{backgroundColor: 'gray'}}
+          />
+        }
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('meetme', () => meetme);
